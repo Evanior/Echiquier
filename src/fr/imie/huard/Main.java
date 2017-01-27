910526d252b5ed51614a9288aa115552f9c23bc2
Huard Timothée <huard.tim@laposte.net>
2017-01-27 16:45:25 +0100

    modification de la classe Echiquier

--- src/fr/imie/huard/Main.java
+++ src/fr/imie/huard/Main.java
@@ -1,7 +1,23 @@
 package fr.imie.huard;
 
+import fr.imie.huard.echiquier.Echiquier;
+import fr.imie.huard.echiquier.Position;
+import fr.imie.huard.piece.*;
+
 /**
  * Created by huard.cdi04 on 27/01/2017.
  */
 public class Main {
+
+    public static void main(String[] args){
+        Roi roiNoir = new Roi('N',new Position(8,5));
+        Roi roiBlanc = new Roi('B',new Position(1,5));
+        Dame dameNoir = new Dame('N',new Position(8,4));
+        Dame dameBlanc = new Dame('B',new Position(1,4));
+        Echiquier.getInstance().ajouterPiece(roiNoir);
+        Echiquier.getInstance().ajouterPiece(roiBlanc);
+        Echiquier.getInstance().ajouterPiece(dameNoir);
+        Echiquier.getInstance().ajouterPiece(dameBlanc);
+        Echiquier.getInstance().afficher();
+    }
 }
